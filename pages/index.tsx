6a06034b05361a394b81da0d2ecdd002de722fbd
Scott Cameron <mscottcam@gmail.com>
2022-05-28 14:19:29 -0500

    layout better, working on input handling

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,13 @@
-import { Input, Text, Box, Heading, useTheme } from "@chakra-ui/react";
+import { useState, ChangeEvent, KeyboardEventHandler } from "react";
+import {
+  Input,
+  Text,
+  Box,
+  Heading,
+  useTheme,
+  Button,
+  Flex,
+} from "@chakra-ui/react";
 import type { NextPage } from "next";
 import Head from "next/head";
 import { DefenderIllustration } from "../components/DefenderIllustration";
@@ -8,6 +17,36 @@ const Home: NextPage = () => {
   const {
     fonts: { heading, body },
   } = useTheme();
+  const [email, setEmail] = useState("");
+  const [emailIsValid, setEmailIsValid] = useState(true);
+  const [placeholder, setPlaceholder] = useState("");
+  console.log(emailIsValid);
+
+  const onChange = (event: ChangeEvent<HTMLInputElement>) => {
+    console.log("on change");
+    if (!emailIsValid) setEmailIsValid(true);
+    setEmail(event.target.value);
+  };
+
+  const onKeyPress: KeyboardEventHandler<HTMLInputElement> = (event) =>
+    event.key === "Enter" && submit();
+
+  const submit = () => {
+    console.log(email);
+    const isEmailValid = !!email.length && false;
+    if (!isEmailValid) {
+      setEmailIsValid(false);
+      setPlaceholder("Please enter valid email!");
+    }
+    setEmail("");
+  };
+
+  const onClick = () => {
+    setEmail("");
+    setEmailIsValid(true);
+    setPlaceholder("Enter your email");
+  };
+
   return (
     <>
       <Head>
@@ -15,28 +54,25 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={styles.main}>
-        <Heading size="2xl" color="#317491" fontFamily={heading}>
-          More to come!
-        </Heading>
         <DefenderIllustration />
         <Heading
-          size="md"
+          size="2xl"
           color="#317491"
-          width="50rem"
+          fontFamily={heading}
+          width="45rem"
           maxWidth="calc(100vw - 4rem)"
-          fontFamily={body}
         >
-          {
-            "Please give us your email if you'd like to know when our store is back up."
-          }
+          More to come! Please give us your email if youd like to know when our
+          store is back up. -Mañana
         </Heading>
         <Box
           width="40rem"
           maxWidth="calc(100vw - 3rem)"
-          border="2px solid #317491"
+          border={emailIsValid ? "2px solid #317491" : `2px solid #D03228`}
           display="inline-flex"
           flexDirection="column"
           justifyContent="center"
+          marginTop="2rem"
         >
           <Text
             color="#317491"
@@ -60,8 +96,43 @@ const Home: NextPage = () => {
             fontSize="18px"
             margin="0 5px 5px"
             fontWeight={800}
+            value={email}
+            sx={{
+              caretColor: "#D03228",
+            }}
+            onChange={onChange}
+            onClick={onClick}
+            onKeyDown={onKeyPress}
+            placeholder={placeholder}
+            _placeholder={{ opacity: 0.3 }}
           />
         </Box>
+        <Flex
+          justifyContent="flex-end"
+          width="40rem"
+          maxWidth="calc(100vw - 3rem)"
+        >
+          <Button
+            onClick={submit}
+            variant="unstyled"
+            background="#D03228"
+            borderRadius={0}
+            padding="20px"
+            marginTop="5px"
+            display="flex"
+            color="#ebe2d7"
+            alignItems="center"
+            border="2px solid transparent"
+            _hover={{
+              border: "2px solid #D03228",
+              background: "#ebe2d7",
+              color: "#D03228",
+            }}
+            _focus={{ boxShadow: "none" }}
+          >
+            Send it.
+          </Button>
+        </Flex>
       </main>
     </>
   );
